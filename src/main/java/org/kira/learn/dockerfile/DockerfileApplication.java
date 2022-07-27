d89ad2c04961a6cede72d2b0d80241461990252e
zhangchaoqing <zzccqq2016@126.com>
2022-07-27 23:21:07 +0800

    init

---
+++ src/main/java/org/kira/learn/dockerfile/DockerfileApplication.java
@@ -0,0 +1,16 @@
+package org.kira.learn.dockerfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhang chaoqing
+ * Create on 2022/7/26 AT 22:17
+ */
+@SpringBootApplication
+public class DockerfileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerfileApplication.class, args);
+    }
+
+}
